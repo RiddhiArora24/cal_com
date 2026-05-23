bbfdb2db0b320434a3ee13165c98bef0ffc2e321
RiddhiArora24 <ariddhi569@gmail.com>
2026-05-23 23:35:31 +0530

    final_push

--- frontend/src/components/PublicBookingPage.jsx
+++ frontend/src/components/PublicBookingPage.jsx
@@ -14,7 +14,7 @@ export default function PublicBookingPage({ eventSlug, navigateBack }) {
   const [dynamicTimeSlots, setDynamicTimeSlots] = useState([]);
   const [takenSlots, setTakenSlots] = useState([]);
 
-  const API_URL = 'http://localhost:5000/api';
+  const API_URL = 'https://cal-com-backend-k159.onrender.com/api';
 
   useEffect(() => {
     axios.get(`${API_URL}/event-types`)
